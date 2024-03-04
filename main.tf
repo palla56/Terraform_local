219183e4d2f9b4ec6ba36dc9a027054d0a5bd3bf
palla56 <pallakasbe56@gmail.com>
2024-03-04 15:25:41 +0530

    change main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-# main.tf
-
-# Define the provider configuration for Docker
-provider "docker" {
-  # Specify the source address for the Docker provider
-  source = "kreuzwerker/docker"
-  
-  # Docker connection details
-  host = "tcp://localhost:2375"  # Specify the address of the Docker daemon
-}
-
-# Define a Docker container resource
-resource "docker_container" "example" {
-  # Specify the name of the Docker container
-  name = "my-container"
-
-  # Define the container configuration
-  image = "nginx:latest"  # Specify the Docker image to pull from the registry
-
-  # Optionally, define additional container settings such as ports, volumes, environment variables, etc.
-  # Example:
-  # ports {
-  #   internal = 80
-  #   external = 8080
-  # }
-
-  # volumes {
-  #   container_path = "/app"
-  #   host_path      = "/data"
-  # }
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:precise"
 }
