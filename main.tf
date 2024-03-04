e61d39b90f5d5237c42b378dddf084f549d751c6
palla56 <pallakasbe56@gmail.com>
2024-03-04 15:28:50 +0530

    1

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:precise"
+resource "docker_image" "zoo" {
+  name = "zoo"
+  build {
+    context = "."
+    tag     = ["zoo:develop"]
+    build_arg = {
+      foo : "zoo"
+    }
+    label = {
+      author : "zoo"
+    }
+  }
 }
